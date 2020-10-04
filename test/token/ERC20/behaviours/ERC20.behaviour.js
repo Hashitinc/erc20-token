c3f4ea93b165ad01b6d0306192c39230c1c35673
vittominacori <vittoriominacori@gmail.com>
2020-10-04 14:33:31 +0200

    fix: remove mint test from ERC20 behaviour

--- test/token/ERC20/behaviours/ERC20.behaviour.js
+++ test/token/ERC20/behaviours/ERC20.behaviour.js
@@ -428,43 +428,6 @@ function shouldBehaveLikeERC20 (_name, _symbol, _decimals, [owner, recipient, an
       });
     });
   });
-
-  describe('mint', function () {
-    const initialSupply = new BN(initialBalance);
-    const amount = new BN(50);
-
-    it('rejects a null account', async function () {
-      await expectRevert(
-        this.token.mint(ZERO_ADDRESS, amount),
-        'ERC20: mint to the zero address',
-      );
-    });
-
-    describe('for a non null account', function () {
-      beforeEach('minting', async function () {
-        const { logs } = await this.token.mint(recipient, amount);
-        this.logs = logs;
-      });
-
-      it('increments totalSupply', async function () {
-        const expectedSupply = initialSupply.add(amount);
-        (await this.token.totalSupply()).should.be.bignumber.equal(expectedSupply);
-      });
-
-      it('increments recipient balance', async function () {
-        (await this.token.balanceOf(recipient)).should.be.bignumber.equal(amount);
-      });
-
-      it('emits Transfer event', async function () {
-        const event = expectEvent.inLogs(this.logs, 'Transfer', {
-          from: ZERO_ADDRESS,
-          to: recipient,
-        });
-
-        event.args.value.should.be.bignumber.equal(amount);
-      });
-    });
-  });
 }
 
 module.exports = {
